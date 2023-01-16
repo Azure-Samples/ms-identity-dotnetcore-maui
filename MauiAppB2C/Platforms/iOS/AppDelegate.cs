276dfd56a8a3a9658d9d9fd7f976c5150beeaad5
bearyung <bearyung@gmail.com>
2023-01-16 17:46:45 +1030

    aaa

--- MauiAppB2C/Platforms/iOS/AppDelegate.cs
+++ MauiAppB2C/Platforms/iOS/AppDelegate.cs
@@ -2,12 +2,22 @@
 // Licensed under the MIT License.
 using Foundation;
 using MauiB2C.MSALClient;
+using Microsoft.Identity.Client;
 using UIKit;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MauiB2C;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        // Initialize MSAL and platformConfig is set
+        IAccount existinguser = Task.Run(async () => await PublicClientSingleton.Instance.MSALClientHelper.InitializePublicClientAppAsync()).Result;
+
+        return base.FinishedLaunching(application, launchOptions);
+    }
 }
