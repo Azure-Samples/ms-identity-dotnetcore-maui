0bc21ed551be11476e1561eb41a2d976badbe96c
bearyung <bearyung@gmail.com>
2023-01-16 17:48:23 +1030

    Revert "aaa"
    This reverts commit 276dfd56a8a3a9658d9d9fd7f976c5150beeaad5.

--- MauiAppB2C/Platforms/iOS/AppDelegate.cs
+++ MauiAppB2C/Platforms/iOS/AppDelegate.cs
@@ -2,22 +2,12 @@
 // Licensed under the MIT License.
 using Foundation;
 using MauiB2C.MSALClient;
-using Microsoft.Identity.Client;
 using UIKit;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace MauiB2C;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-
-    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
-    {
-        // Initialize MSAL and platformConfig is set
-        IAccount existinguser = Task.Run(async () => await PublicClientSingleton.Instance.MSALClientHelper.InitializePublicClientAppAsync()).Result;
-
-        return base.FinishedLaunching(application, launchOptions);
-    }
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
